d6308ab98676032785f28421d365b9e9605a8534
nfsenharo <nfsenharo@gmail.com>
2026-08-02 23:18:06 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const button = document.querySelector("button");
 
+    button.addEventListener("click", function () {
+        alert("یہاں آپ اپنا Daraz Affiliate Link شامل کریں گے۔");
+    });
+});
